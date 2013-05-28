76d32c192191deeb4e96fb11e56d0e06310eefef
Jordy Langen <jordy.langen@mediaan.com>
2013-05-28 16:13:04 +0200

    Updated files based on sample.

--- jquery.bulletchart.js
+++ jquery.bulletchart.js
@@ -1,56 +1,108 @@
 ﻿var bulletChartClasses = {
     container: 'visual-progress-container',
     total: 'visual-progress-total',
+    summary: 'visual-progress-summary-current',
     current: 'visual-progress-current',
     baseline: 'visual-progress-baseline',
     unallocated: 'visual-progress-unallocated',
     underallocated: 'visual-progress-underallocated',
-    overallocted: 'visual-progress-overallocated'
+    overallocted: 'visual-progress-overallocated',
+    overallocatedTotal: 'visual-progress-overallocted-total'
 };
 
 (function ($) {
-    
+
     $.fn.bulletChart = function (options) {
+
+        if (!String.prototype.format) {
+            String.prototype.format = function () {
+                var args = arguments;
+                return this.replace(/{(\d+)}/g, function (match, number) {
+                    return typeof args[number] != 'undefined'
+                      ? args[number]
+                      : match;
+                });
+            };
+        }
+
+        options = $.extend({
+            subtitleFormat: 'of {0}h',
+        }, options);
 
         var self = this;
 
-        var current = options.current;
-        var total = options.total;
-        var baseline = options.baseline;
-
-        function validateOptions() {
-            if (!current)
-                throw 'Options must contains an element \'current\'.';
-            if (!total)
-                throw 'Options must contains an element \'total\'.';
-            if (baseline == '')
-                baseline = undefined;
-
+        if (!self.hasClass(bulletChartClasses.container)) {
             self.addClass(bulletChartClasses.container);
         }
 
+        function validateOptions() {
+            if (!options.current)
+                throw 'Options must contains an element \'current\'.';
+            if (!options.total)
+                throw 'Options must contains an element \'total\'.';
+            if (options.baseline == '')
+                options.baseline = undefined;
+        }
+
         function resolveOptions() {
-            if (current.constructor === String) {
-                current = current.replace(',', '.');
-                current = parseFloat(current);
+            if (options.current.constructor === String) {
+                options.current = options.current.replace(',', '.');
+                options.current = parseFloat(options.current);
             }
-            if (total.constructor === String) {
-                total = total.replace(",", ".");
-                total = parseFloat(total);
+            if (options.total.constructor === String) {
+                options.total = options.total.replace(",", ".");
+                options.total = parseFloat(options.total);
             }
-            if (baseline && baseline.constructor === String) {
-                baseline = baseline.replace(',', '.');
-                baseline = parseFloat(baseline);
+            if (options.baseline && options.baseline.constructor === String) {
+                options.baseline = options.baseline.replace(',', '.');
+                options.baseline = parseFloat(options.baseline);
             }
         }
 
-        function createCurrentAndTotal() {
+        function createHtml() {
 
-            var width = (current / total) * 100;
+            var totalWidth = self.width();
+
+            if (options.title) {
+                var title = $('<h4>').text(options.title);
+                self.append(title);
+            }
+
+            // create the summary, containers and subtitle
+            var summary = $('<div>').addClass(bulletChartClasses.summary).text(options.current);
+            self.append(summary);
+
+            var remainigWidth = totalWidth - summary.width();
+            var chartWidth = (Math.floor((remainigWidth / totalWidth) * 100) - 3) + '%';
+
+            var chartContainer = $('<div>').css({ display: 'inline-block', width: chartWidth });
+            self.append(chartContainer);
+
+            var animationContainer = $('<div>').addClass(bulletChartClasses.container);
+            chartContainer.append(animationContainer);
+
+            createBulletCharts(animationContainer);
+
+            var subtitle = !options.baseline ? options.subtitleFormat.format(options.total) : options.subtitleFormat.format(options.total, options.baseline);
+            var subtitleElement = $('<div>').text(subtitle);
+            chartContainer.append(subtitleElement);
+        }
+
+        function createBulletCharts(container) {
+            if (!options.baseline) {
+                createDoubleBulletChart(container);
+            } else {
+                createTripleBulletChart(container);
+            }
+        }
+
+        function createDoubleBulletChart(container) {
+
+            var width = (options.current / options.total) * 100;
             var totalValue = $('<div>');
             var currentValue = $('<div>');
 
-            if (current <= total) {
+            if (options.current <= options.total) {
                 totalValue
                     .addClass(bulletChartClasses.total)
                     .addClass(bulletChartClasses.unallocated)
@@ -62,8 +114,8 @@
                     .css('width', width + '%');
 
                 totalValue.append(currentValue);
-                self.append(totalValue);
-                animateSlideToLeft(self);
+                container.append(totalValue);
+                animateSlideToLeft(container);
             } else {
                 width = 100 - (width - 100);
 
@@ -78,24 +130,24 @@
                     .css('width', width + '%');
 
                 currentValue.append(totalValue);
-                self.append(currentValue);
-                animateSlideToLeft(self);
+                container.append(currentValue);
+                animateSlideToLeft(container);
             }
         }
 
-        function visualizeCurrentTotalAndBaseline() {
-
+        function createTripleBulletChart(container) {
             var totalValue = $('<div>');
             var currentValue = $('<div>');
 
-            var firstLayer = baseline > total ? bulletChartClasses.baseline : bulletChartClasses.total;
+            var firstLayer = options.baseline > options.total ? bulletChartClasses.baseline : bulletChartClasses.overallocatedTotal;
             var firstElement = $('<div>').addClass(firstLayer).css('width', '100%');
 
             if (firstLayer == bulletChartClasses.baseline) {
 
-                if (current <= total) {
-                    var totalWidth = (total / baseline) * 100;
-                    var currentWidth = (current / total) * 100;
+                if (options.current <= options.total) {
+                    // baseline > total > current
+                    var totalWidth = (options.total / options.baseline) * 100;
+                    var currentWidth = (options.current / options.total) * 100;
 
                     totalValue
                         .addClass(bulletChartClasses.total)
@@ -109,11 +161,35 @@
 
                     totalValue.append(currentValue);
                     firstElement.append(totalValue);
-                    animateSlideToLeft(self);
+                    animateSlideToLeft(container);
+
+                } else if (options.current > options.baseline) {
+                    // current > baseline > total
+                    firstElement.removeClass(firstLayer);
+                    firstElement.addClass(bulletChartClasses.current).addClass(bulletChartClasses.overallocted);
+
+                    var totalWidth = (options.total / options.current) * 100;
+                    var currentWidth = (options.total / options.baseline) * 100;
+
+                    currentValue
+                        .addClass(bulletChartClasses.baseline)
+                        .addClass(bulletChartClasses.overallocted)
+                        .css('width', currentWidth + '%');
+
+                    totalValue
+                        .addClass(bulletChartClasses.total)
+                        .addClass(bulletChartClasses.overallocted)
+                        .css('width', totalWidth + '%');
+
+                    currentValue.append(totalValue);
+                    firstElement.append(currentValue);
+                    animateSlideToLeft(container);
+
 
                 } else {
-                    var totalWidth = (total / current) * 100;
-                    var currentWidth = (total / baseline) * 100;
+                    // baseline > current > total
+                    var totalWidth = (options.total / options.current) * 100;
+                    var currentWidth = (options.total / options.baseline) * 100;
 
                     currentValue
                         .addClass(bulletChartClasses.current)
@@ -127,11 +203,53 @@
 
                     currentValue.append(totalValue);
                     firstElement.append(currentValue);
-                    animateSlideToLeft(self);
+                    animateSlideToLeft(container);
+                }
+            } else {
+                if (options.current <= options.total) {
+                    // total > baseline > current
+                    var totalWidth = (options.baseline / options.total) * 100;
+                    var currentWidth = (options.current / options.total) * 100;
+
+                    totalValue
+                        .addClass(bulletChartClasses.baseline)
+                        .addClass(bulletChartClasses.unallocated)
+                        .css('width', totalWidth + '%');
+
+                    currentValue
+                        .addClass(bulletChartClasses.current)
+                        .addClass(bulletChartClasses.underallocated)
+                        .css('width', currentWidth + '%');
+
+                    totalValue.append(currentValue);
+                    firstElement.append(totalValue);
+                    animateSlideToLeft(container);
+
+                } else if(options.current > options.total) {
+                    // current > total > baseline
+                    firstElement.removeClass(firstLayer);
+                    firstElement.addClass(bulletChartClasses.current).addClass(bulletChartClasses.overallocted);
+
+                    var totalWidth = (options.total / options.current) * 100;
+                    var currentWidth = (options.total / options.current) * 100;
+
+                    currentValue
+                        .addClass(bulletChartClasses.total)
+                        .addClass(bulletChartClasses.overallocted)
+                        .css('width', currentWidth + '%');
+
+                    totalValue
+                        .addClass(bulletChartClasses.baseline)
+                        .addClass(bulletChartClasses.overallocted)
+                        .css('width', totalWidth + '%');
+
+                    currentValue.append(totalValue);
+                    firstElement.append(currentValue);
+                    animateSlideToLeft(container);
                 }
             }
 
-            self.append(firstElement);
+            container.append(firstElement);
         }
 
         function animateSlideToLeft(element) {
@@ -146,12 +264,7 @@
 
         validateOptions();
         resolveOptions();
-
-        if (!baseline) {
-            createCurrentAndTotal();
-        } else {
-            visualizeCurrentTotalAndBaseline();
-        }
-    };
+        createHtml();
+    }
 
 }(jQuery));
